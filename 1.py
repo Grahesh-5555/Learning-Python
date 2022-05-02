91bfe404c9e79a619a4cc570414d16c15db8e17c
Grahesh-5555 <104724050+Grahesh-5555@users.noreply.github.com>
2022-05-02 05:51:04 -0800

    changes

---
+++ 1.py
@@ -0,0 +1,30 @@
+# Did you know that you can name stuff
+
+
+name = "Grash"
+print(name)
+
+# Or
+
+print("Grash")
+
+print ("Grahesh")
+
+
+print ("lord zen") t
+# You can also do this:
+age = 14
+name2 = "Advik"
+print("Hello my name is %s" % name)
+
+print("Hello my name is %s" % name2)
+
+name93843849349 = "grahesh"
+
+print("Hello my name is %s" % name93843849349)
+
+name_big = name + name2
+print(name_big)
+
+name_big = name + "zenits"
+print(name_big)
